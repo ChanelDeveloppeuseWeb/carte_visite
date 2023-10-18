9c01264ccf47f4904ad465fba30cf6fc9556b147
kazi kuete charnel <chanelkuete2@gmail.com>
2023-10-18 09:25:48 +0100

    reinitilise

---
+++ script.js
@@ -0,0 +1,63 @@
+// declaration de ma variable etudiants
+
+let etudiants = {
+  nom: "",
+  proffesion: "",
+  date: "",
+  age: "",
+  photo: "",
+};
+
+function validateForm() {
+  // je recupere les donnees du formulaire
+
+  etudiants.nom = document.getElementById("Nom").value;
+  etudiants.proffesion = document.getElementById("Proffession").value;
+  etudiants.date = document.getElementById("Date").value;
+  etudiants.age = document.getElementById("Age").value;
+  etudiants.photo = document.getElementById("Photo").value;
+
+  let image = document.getElementById("Photo").files[0];
+  let render = new FileReader();
+
+  document.querySelector(".content").style.display = "none";
+  document.querySelector("#content").style.display = "block";
+
+  // j affiche mes donnees
+  document.querySelector("#nom").innerHTML = "nom: " + etudiants.nom;
+  document.querySelector("#proffesion").innerHTML =
+    "proffesion: " + etudiants.proffesion;
+  document.querySelector("#date").innerHTML = "date: " + etudiants.date;
+  document.querySelector("#age").innerHTML = "age: " + etudiants.age;
+  document.querySelector("#image").innerHTML = "photo:" + etudiants.photo
+
+  console.log(
+    etudiants.nom,
+    etudiants.proffesion,
+    etudiants.age,
+    etudiants.photo,
+    etudiants.date
+  );
+  render.onload = function (e) {
+    console.log(e.target);
+    render.readAsDataURL(image);
+  };
+
+  document.getElementById("photo").src = render;
+}
+
+function resetForm() {
+  document.getElementById("Nom").value = "";
+  document.getElementById("Proffession").value = "";
+  document.getElementById("Date").value = "";
+  document.getElementById("Age").value = "";
+  document.getElementById("Photo").value = "";
+  document.querySelector(".content").style.display = "block";
+  document.querySelector("#content").style.display = "none";
+}
+
+let btn = document.querySelector("#button");
+btn.addEventListener("click", validateForm);
+
+let btnReset = document.querySelector("#reset");
+btnReset.addEventListener("click", resetForm);
